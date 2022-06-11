4f33e7867c4979cb9e01812dd658bb11e5d36b9f
rafael soares <rafaell.sn@outlook.com>
2022-06-11 13:41:01 -0300

    atualicação de classes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function selecionaItem(elemento){
     const pratoSelecionado = document.querySelector(".selecionado");
     if (pratoSelecionado !== null) {
         pratoSelecionado.classList.remove("selecionado");
+
     }
      
     elemento.classList.add("selecionado");
