d846a5685bb6bdaea5096c7c9fc4c53873e645e8
rafael soares <rafaell.sn@outlook.com>
2022-06-11 00:35:06 -0300

    selecionar item

---
+++ script.js
@@ -0,0 +1,8 @@
+function selecionaItem(elemento){
+    const pratoSelecionado = document.querySelector(".selecionado");
+    if (pratoSelecionado !== null) {
+        pratoSelecionado.classList.remove("selecionado");
+    }
+     
+    elemento.classList.add("selecionado");
+}
