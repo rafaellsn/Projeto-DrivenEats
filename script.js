291d107a3ba22639d6b17b05215d2fca2b28a2b7
rafael soares <rafaell.sn@outlook.com>
2022-06-13 13:07:03 -0300

    atualização selecionar pratos e classe

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
     function selecionaItem (elemento) {
-    let Botaoclicado = document.querySelector(".selecionado");
+    let Botaoclicado = document.querySelector(".caixa1 .selecionado");
     if (Botaoclicado !== null) {
         Botaoclicado.classList.toggle("selecionado");  
     }
     elemento.classList.add("selecionado");
 }
 
-    function selecionaItem2 (elemento) {
-    let Botaoclicado = document.querySelector(".selecionado");
+    function selecionaItem1 (elemento) {
+    let Botaoclicado = document.querySelector(".caixa2 .selecionado");
     if (Botaoclicado !== null) {
         Botaoclicado.classList.toggle("selecionado");  
     }
     elemento.classList.add("selecionado");
 
 }
+function selecionaItem2 (elemento) {
+    let Botaoclicado = document.querySelector(".caixa3 .selecionado");
+    if (Botaoclicado !== null) {
+        Botaoclicado.classList.toggle("selecionado");  
+    }
+    elemento.classList.add("selecionado");
+
+}
+
+
